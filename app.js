e33856b3f9117ca974801bff851b96c3b776e4a6
sardar <97310586+sardar-dev@users.noreply.github.com>
2026-07-13 20:51:12 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 //.join(" ") spcae in " " , space between every word 
 
-
-function rev (name)
-{
-  let namee = name.split("").reverse("").join(" ")
-  return namee 
+function rev(name) {
+  let namee = name.split("").reverse("").join(" ");
+  return namee;
 }
-document.write(rev(` sardar`)) //print on page instead of console
+document.write(rev(` sardar`)); //print on page instead of console
